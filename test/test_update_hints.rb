68fbc079ba57dd32f0593345f1ba968d8c06df70
Julik Tarkhanov <me@julik.nl>
2013-03-22 01:53:22 +0100

    Make version handling more strict
    Ensure pre-release gems do not end up on the list, and the real version returned is indeed the latest one. Previously if a gem got an update on it's previous release (like 2.2 after 3.0 hit) the version fetched would be 2.2 - which is wrong

--- test/test_update_hints.rb
+++ test/test_update_hints.rb
@@ -12,18 +12,27 @@ class TestUpdateHints < Test::Unit::TestCase
   def test_version_check_properly_output
     xml = File.open(File.dirname(__FILE__) + "/sample.xml")
     output = ''
-    flexmock(UpdateHints).should_receive(:open).with("http://rubygems.org/api/v1/gems/tracksperanto.xml").once.and_return(xml)
+    flexmock(UpdateHints::Checker).new_instances.should_receive(:open).with("http://rubygems.org/api/v1/versions/tracksperanto.xml").once.and_return(xml)
     
     UpdateHints.version_check("tracksperanto", "1.2.3", output)
-    assert_equal "Your version of tracksperanto is probably out of date\n(the current version is 2.4.1, but you have 1.2.3).\nPlease consider updating (run `gem update tracksperanto`)\n", output
+    assert_equal "Your version of tracksperanto is probably out of date\n(the current version is 3.2.2, but you have 1.2.3).\nPlease consider updating (run `gem update tracksperanto`)\n", output
   end
   
   def test_version_check_stays_silent_on_success
     xml = File.open(File.dirname(__FILE__) + "/sample.xml")
     output = ''
-    flexmock(UpdateHints).should_receive(:open).with("http://rubygems.org/api/v1/gems/tracksperanto.xml").once.and_return(xml)
-    UpdateHints.version_check("tracksperanto", "2.4.1", output)
+    flexmock(UpdateHints::Checker).new_instances.should_receive(:open).with("http://rubygems.org/api/v1/versions/tracksperanto.xml").once.and_return(xml)
+    UpdateHints.version_check("tracksperanto", "3.2.2", output)
     assert_equal "", output
+  end
+  
+  def test_sorts_proper_rails_gem_on_top
+    xml = File.open(File.dirname(__FILE__) + "/rails_versions.xml")
+    output = ''
+    flexmock(UpdateHints::Checker).new_instances.should_receive(:open).with("http://rubygems.org/api/v1/versions/rails.xml").once.and_return(xml)
+    # Latest version released is 2.38.1 but the latest release gem is 3.2.2. It also has 4.0.0.pre which is perfect for our test
+    UpdateHints.version_check("rails", "3.1.0", output)
+    assert_equal "Your version of rails is probably out of date\n(the current version is 3.2.13, but you have 3.1.0).\nPlease consider updating (run `gem update rails`)\n", output
   end
   
 end
